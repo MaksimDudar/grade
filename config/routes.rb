0a212049ff09794d29faa2c82d23ef9b3050bfc3
MaksimDudar <maksimdudar1@gmail.com>
2022-10-19 12:57:09 +0300

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root "home#index"
+
   resources :cards
   resources :mastercars
   resources :masters
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
