b86ac584473e192c79ebd47e16e8d5918d713fd6
MaksimDudar <maksimdudar1@gmail.com>
2022-10-22 22:48:02 +0300

    add card

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,10 @@
 module CardsHelper
+  def range2
+    arr = []
+    @cards.each do |card|
+    arr << card.rating
+    end
+    f = arr.inject{ |sum, el| sum + el }.to_f / arr.size
+    f.round(2)
+  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,3 @@
 class Card < ApplicationRecord
+  validates  :strana, :strana1, :truck, :driver, :manager, :rating, presence: true
 end
